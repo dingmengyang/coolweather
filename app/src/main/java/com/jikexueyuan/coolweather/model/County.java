0bd1eae8ae8d140ec5c8e0516b66e723a725e1cf
jason01 <806770578@qq.com>
2016-11-27 16:26:47 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/jikexueyuan/coolweather/model/County.java
@@ -0,0 +1,41 @@
+package com.jikexueyuan.coolweather.model;
+
+
+public class County {
+    private int id;
+    private String county_name;
+    private String county_code;
+    private int city_id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCity_id(int city_id) {
+        this.city_id = city_id;
+    }
+
+    public void setCounty_code(String county_code) {
+        this.county_code = county_code;
+    }
+
+    public void setCounty_name(String county_name) {
+        this.county_name = county_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCity_id() {
+        return city_id;
+    }
+
+    public String getCounty_code() {
+        return county_code;
+    }
+
+    public String getCounty_name() {
+        return county_name;
+    }
+}
